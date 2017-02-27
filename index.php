780cadd7dadeb78aa217b8fb1cdf7f55a5b18a0e
wuoshiwzm <wuoshiwzm@hotmail.com>
2017-02-27 18:39:16 +0800

    test

--- index.php
+++ index.php
@@ -17,8 +17,21 @@ require_once ('woo/mapper/VenueMapper.php');
 require_once ('woo/mapper/Mapper.php');
 
 
-\woo\base\ApplicationRegistry::setDSN('mysql:host=localhost;dbname=pattern');
+//\woo\base\ApplicationRegistry::setDSN('mysql:host=localhost;dbname=pattern');
 //\woo\base\ApplicationRegistry::setDSN('mysql:username=root');
 $mapper = new \woo\mapper\VenueMapper();
-$venue = $mapper->find(12);
- print_r($venue);
+//$venue = $mapper->find(12);
+
+$venue = new \woo\domain\Venue();
+$venue->setName('test set name');
+$mapper->insert($venue);
+$venue = $mapper->find($venue->getId());
+print_r($venue);
+
+
+
+
+
+
+
+print_r($venue);
